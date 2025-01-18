a899f69dc524eb4cf5d3f4ad631d719400623cd2
sherniyaze <sherniyazerkin@gmail.com>
2025-01-18 19:08:30 +0500

    Эта задача про синтаксис и пробелы при написании кода + переменные типа инт и стринг + комменты

---
+++ pythonsyntax.py
@@ -0,0 +1,15 @@
+if 5 > 2:
+    print("Five is greater than two!")
+#it'll be correct because we used indentation
+#but
+#if 5 > 2:
+#print("Five is greater than two!") and
+#
+#if 5 > 2:
+#  print("Five is greater than two!") 
+#if 5 > 2:
+#     print("Five is greater than two!")
+# won't work because of problems with indentation
+
+x = 5
+y = "Hello, World!"
