677f95547432834b52608ff192938449aeb90e9e
sherniyaze <sherniyazerkin@gmail.com>
2025-01-18 20:19:46 +0500

    Урок о названиях переменных(о том какие могут быть и не могут быть названиями переменных)

---
+++ pythonVarnames.py
@@ -0,0 +1,25 @@
+#  переменные могут иметь короткое имя
+#  (такие как х и у) или более описательное 
+#  имя(age, carname, total_volume).
+#  Правила для пайтон переменных:
+# *A variable name must start with a letter 
+#    or the unberscore character
+# *A variable name cannot start with a number
+# *A variable name can only contain alpha-numeric 
+#   characters and underscores(A-z, 0-9, _)
+# *Variable names are case-sensitive
+#   (age, Age and AGE are three different variables)
+# *A variable name cannot be any of the Python keywords
+
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# illegal variable names:
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
