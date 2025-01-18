1e88f0934c0674fb9005cd8f896b37782b0b55d6
sherniyaze <sherniyazerkin@gmail.com>
2025-01-18 21:06:40 +0500

    восьмое задание про глобальные переменные

---
+++ 8thTaskGlobalVar.py
@@ -0,0 +1,16 @@
+x = "awesome"
+
+def myfunc():
+    print("Python is " + x)
+
+myfunc()
+
+x = "awesome"
+
+def mynewfunc():
+    x = "fantastic"
+    print("Python is " + x)
+
+mynewfunc()
+
+print("Python is " + x)
