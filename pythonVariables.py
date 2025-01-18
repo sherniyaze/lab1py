2f7315712bb78fc62bdca39882aa06159b56293b
sherniyaze <sherniyazerkin@gmail.com>
2025-01-18 19:49:50 +0500

    Четвертая задача про важность написания названий переменных, типы переменных и замена их значений

---
+++ pythonVariables.py
@@ -0,0 +1,31 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+
+X = 4 #X is int type variable
+X = "Sally" #and now X is str type
+print(X)
+
+NewX = str(3) #X will be '3'
+NewY = int(3) #Y will be 3
+Z = float(3) #Z will be 3.0
+
+#We can get the data type of a variable 
+#with the type() function
+
+print(type(x))
+print(type(y))
+print(type(X))
+
+#and string variables can be declared 
+#either by using single or double quotes
+
+B = "JOHN"
+#is the same as
+B = 'JOHN'
+
+# WARNING!!!  Variable names are case-sensitive
+a = 4
+A = "SALLY"
+#A will not overwrite a
